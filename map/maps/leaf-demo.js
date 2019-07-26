3d9537c8090102d1b31c458e653944a63f6ca449
manmolecular <regwebghost@yandex.ru>
2019-07-27 02:19:22 +0700

    Add bootstrap "Show More" information spoiler

--- map/maps/leaf-demo.js
+++ map/maps/leaf-demo.js
@@ -43,6 +43,15 @@ for (var i = 0; i < markers.length; ++i) {
   let censysAdditionalInfo = 'https://censys.io/ipv4/' + markers[i].ip;
   let googleMapsInfo = 'https://www.google.com/maps/search/?api=1&query=' + markers[i].lat + ',' + markers[i].lng;
 
+  let allLinks = '<a id="LinkToHost" title="Link to host" href="#" onclick="openInNewTab(\'' + HostAddress + '\');return false;"><b>Open</b></a>' + 
+  '<br/>' + 
+  '<a id="LinkToShodan" title="Link to Shodan" href="#" onclick="openInNewTab(\'' + shodanAdditionalInfo + '\');return false;"><b>Show in Shodan</b></a>' + 
+  '<br/>' + 
+  '<a id="LinkToCensys" title="Link to Censys" href="#" onclick="openInNewTab(\'' + censysAdditionalInfo + '\');return false;"><b>Show in Censys</b></a>' +
+  '<br/>' +
+  '<a id="LinkToGoogleMaps" title="Link to Google Maps" href="#" onclick="openInNewTab(\'' + googleMapsInfo + '\');return false;"><b>Show in Google Maps</b></a>';
+
+
   let context = {
     vendor: markers[i].vendor || "Not detected",
     product: markers[i].product || "Not detected",
@@ -51,18 +60,10 @@ for (var i = 0; i < markers.length; ++i) {
     port: markers[i].port,
     protocol: markers[i].proto || "Not detected",
     latitude: Math.round(markers[i].lat * 1000) / 1000,
-    longitude: Math.round(markers[i].lng * 1000) / 1000
+    longitude: Math.round(markers[i].lng * 1000) / 1000,
+    content: allLinks
   }
   var popup = template(context);
-
-  var popup = popup +       
-  '<a id="LinkToHost" title="Link to host" href="#" onclick="openInNewTab(\'' + HostAddress + '\');return false;"><b>Open ' + HostAddress + '</b></a>' + 
-  '<br/>' + 
-  '<a id="LinkToShodan" title="Link to Shodan" href="#" onclick="openInNewTab(\'' + shodanAdditionalInfo + '\');return false;"><b>Show information from Shodan</b></a>' + 
-  '<br/>' + 
-  '<a id="LinkToCensys" title="Link to Censys" href="#" onclick="openInNewTab(\'' + censysAdditionalInfo + '\');return false;"><b>Show information from Censys</b></a>' +
-  '<br/>' +
-  '<a id="LinkToGoogleMaps" title="Link to Google Maps" href="#" onclick="openInNewTab(\'' + googleMapsInfo + '\');return false;"><b>Show information from Google Maps</b></a>';
 
   var m = L.marker([markers[i].lat, markers[i].lng], {
           icon: myIcon

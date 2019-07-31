07d087d9c6abd0e4f7ee9f9bb05d72b457f69343
manmolecular <regwebghost@yandex.ru>
2019-07-31 17:27:38 +0700

    Update save path for markers

--- grinder/mapmarkers.py
+++ grinder/mapmarkers.py
@@ -3,6 +3,7 @@
 from json import dumps
 
 from grinder.defaultvalues import DefaultValues
+from pathlib import Path
 
 
 class MapMarkers:
@@ -12,6 +13,10 @@ class MapMarkers:
     def update_markers(self, results: list, map_directory=None) -> None:
         if not map_directory:
             map_directory = DefaultValues.MARKERS_DIRECTORY
-        with open(f"{map_directory}/maps/markers.js", mode="w") as js_markers:
+        with open(Path(".")
+                  .joinpath(map_directory)
+                  .joinpath("static")
+                  .joinpath("data")
+                  .joinpath("markers.js"), mode="w") as js_markers:
             js_dump_results = dumps(results)
             js_markers.write("var markers = {markers}".format(markers=js_dump_results))

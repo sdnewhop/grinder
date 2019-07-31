69414a56ed4eda5f2dc7abf9084565a0fbdf2a09
manmolecular <regwebghost@yandex.ru>
2019-07-31 19:18:34 +0700

    Fetch JSON from API

--- map/static/custom/grinder.js
+++ map/static/custom/grinder.js
@@ -19,41 +19,47 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/
 
 var source = $("#grinder-popup").html();
 var template = Handlebars.compile(source);
-
 var markerClusters = L.markerClusterGroup();
-for (var i = 0; i < markers.length; ++i) {
-  let proto = (markers[i].port === (443 || 8443)) ? 'https://' : 'http://';
-  let context = {
-      basic: {
-        vendor: markers[i].vendor || 'Not detected',
-        product: markers[i].product || 'Not detected',
-        version: markers[i].additional_info || 'Not detected',
-        ip: markers[i].ip,
-        port: markers[i].port,
-        protocol: markers[i].proto || 'Not detected',
-        latitude: Math.round(markers[i].lat * 1000) / 1000,
-        longitude: Math.round(markers[i].lng * 1000) / 1000,
-      },
-      api: {
-        raw: 'api/viewraw/' + i.toString(),
-      },
-      additionalContent: {
-        host: proto + markers[i].ip + ':' + markers[i].port,
-        shodan: 'https://www.shodan.io/host/' + markers[i].ip,
-        censys: 'https://censys.io/ipv4/' + markers[i].ip,
-        zoomeye: 'https://www.zoomeye.org/searchResult?q=' + markers[i].ip,
-        googlemaps: 'https://www.google.com/maps/search/?api=1&query=' + markers[i].lat + ',' + markers[i].lng,
-        iplookup: 'https://extreme-ip-lookup.com/' + markers[i].ip,
-      },
-  }
 
-  let popup = template(context);
-  let m = L.marker([markers[i].lat, markers[i].lng], {
-          icon: myIcon
-      })
-      .bindPopup(popup);
+fetch(window.location.origin + '/api/viewall')
+  .then(function(response) {
+      return response.json();
+  })
+  .then(function(markers) {
+      for (var i = 0; i < markers.length; ++i) {
+          let proto = (markers[i].port === (443 || 8443)) ? 'https://' : 'http://';
+          let context = {
+              basic: {
+                  vendor: markers[i].vendor || 'Not detected',
+                  product: markers[i].product || 'Not detected',
+                  version: markers[i].additional_info || 'Not detected',
+                  ip: markers[i].ip,
+                  port: markers[i].port,
+                  protocol: markers[i].proto || 'Not detected',
+                  latitude: Math.round(markers[i].lat * 1000) / 1000,
+                  longitude: Math.round(markers[i].lng * 1000) / 1000,
+              },
+              api: {
+                  raw: 'api/viewraw/' + i.toString(),
+              },
+              additionalContent: {
+                  host: proto + markers[i].ip + ':' + markers[i].port,
+                  shodan: 'https://www.shodan.io/host/' + markers[i].ip,
+                  censys: 'https://censys.io/ipv4/' + markers[i].ip,
+                  zoomeye: 'https://www.zoomeye.org/searchResult?q=' + markers[i].ip,
+                  googlemaps: 'https://www.google.com/maps/search/?api=1&query=' + markers[i].lat + ',' + markers[i].lng,
+                  iplookup: 'https://extreme-ip-lookup.com/' + markers[i].ip,
+              },
+          }
 
-  markerClusters.addLayer(m);
-}
+          let popup = template(context);
+          let m = L.marker([markers[i].lat, markers[i].lng], {
+                  icon: myIcon
+              })
+              .bindPopup(popup);
 
-map.addLayer(markerClusters);
+          markerClusters.addLayer(m);
+      }
+
+      map.addLayer(markerClusters);
+  });
--- map/static/custom/helpers.js
+++ map/static/custom/helpers.js
@@ -1,4 +1,4 @@
 function openInNewTab(url) {
-    var win = window.open(url, '_blank');
-    win.focus();
-  }
+  var win = window.open(url, '_blank');
+  win.focus();
+}

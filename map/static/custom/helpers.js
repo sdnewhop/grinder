8764075b2d9ab9f61eac8162cd7969a0662b0e4e
Anton Nikolaev <regwebghost@yandex.ru>
2019-09-18 23:55:02 +0700

    Check case when innerHTML is unavailable to update (user close host window)

--- map/static/custom/helpers.js
+++ map/static/custom/helpers.js
@@ -6,13 +6,17 @@ function openInNewTab(url) {
 function ping(index) {
     document.getElementById('host-ping-status').innerHTML = 'awaiting...';
     document.getElementById('host-ping-status').style.color = '#d99f00';
-    fetch(window.location.origin + '/api/viewraw/' + index + '/ping').then(function(response) {
-        document.getElementById('host-ping-status').innerHTML = 'awaiting response...';
-        document.getElementById('host-ping-status').style.color = '#d99f00';
-        return response.json();
+    fetch(`${window.location.origin}/api/viewraw/${index}/ping`).then(function(response) {
+        if (document.getElementById('host-ping-status') !== null) {
+            document.getElementById('host-ping-status').innerHTML = 'awaiting response...';
+            document.getElementById('host-ping-status').style.color = '#d99f00';
+            return response.json();
+        }
     }).then(function(pingStatus) {
         console.log(pingStatus);
-        document.getElementById('host-ping-status').innerHTML = pingStatus.status ? pingStatus.status : pingStatus.error;
-        document.getElementById('host-ping-status').style.color = (pingStatus.status === 'online') ? 'green' : 'red';
+        if (document.getElementById('host-ping-status') !== null) {
+            document.getElementById('host-ping-status').innerHTML = pingStatus.status ? pingStatus.status : pingStatus.error;
+            document.getElementById('host-ping-status').style.color = (pingStatus.status === 'online') ? 'green' : 'red';
+        }
     });
 }

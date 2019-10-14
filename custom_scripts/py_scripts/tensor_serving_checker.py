d64757ca7b431e5f91832c5f1f854a493d802e25
Anton Nikolaev <regwebghost@yandex.ru>
2019-10-15 00:43:56 +0700

    Update detect logic

--- custom_scripts/py_scripts/tensor_serving_checker.py
+++ custom_scripts/py_scripts/tensor_serving_checker.py
@@ -17,14 +17,24 @@ def main(host_info: dict, timeout: int = 3) -> dict:
     }
 
     try:
-        url = f"http://{host_info.get('ip')}:{host_info.get('port')}/v1/models/versions/metadata"
+        url = f"http://{host_info.get('ip')}:{host_info.get('port')}/v1/models"
         resp = get(url, verify=False, timeout=timeout).text
     except (TimeoutError, ConnectionError, ConnectTimeout, ContentDecodingError):
         output.update({"status": "Timeout Error Was Caught"})
         return output
-    if "404" in resp:
-        status = "Not TensorFlow Serving Server"
-    else:
+    if "Missing model name in request" in resp:
         status = "Found TensorFlow Serving Server"
+    elif "404" not in resp:
+        status = "Possibly TensorFlow Serving Server"
+    else:
+        status = "Not TensorFlow Serving Server"
     output.update({"html": resp, "status": status})
     return output
+
+
+if __name__ == "__main__":
+    host_info = {
+        "ip": "localhost",
+        "port": 8501
+    }
+    print(main(host_info))

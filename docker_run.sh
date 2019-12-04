95dbe3123692bd38601aa86f0b20580568520f0e
Simon Kirillov <svkirillov3@gmail.com>
2019-12-04 20:58:57 +0700

    Add option for specify how much of the available CPU resources a container can use

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env sh
 
+usage() {
+	echo "Usage: $(basename $0) [--cpus <quantity-of-cpu>] [-h | --help]"
+}
+
+user_cpu_count=0
+cpu_count=0
 pwd=$(pwd)
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		"-h"|"--help") usage; exit 0 ;;
+		"--cpus") user_cpu_count=$2; shift 2 ;;
+		*) usage; exit 0 ;;
+	esac
+done
+
+case "$(uname)" in
+	"Linux") cpu_count=$(grep -c 'processor' /proc/cpuinfo) ;;
+	"Darwin") cpu_count=$(sysctl -n hw.logicalcpu) ;;
+esac
+
+if [ $user_cpu_count -gt $cpu_count ]; then
+	user_cpu_count=$cpu_count
+fi
+
+if [ $user_cpu_count -eq 0 ]; then
+	user_cpu_count=$(echo "$cpu_count / 2" | bc)
+fi
 
 if [ ! -e database.db ]; then
 	touch ./database.db
@@ -11,6 +38,7 @@ mkdir -p ./map/static/data/
 
 docker run -it --rm \
 	-p 5000:5000 \
+	--cpus="$user_cpu_count" \
 	--volume ${pwd}/database.db:/app/database.db \
 	--volume ${pwd}/results:/app/results \
 	--volume ${pwd}/map/static/data:/app/map/static/data \

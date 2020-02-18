b5c1cb8df578e2546840f16a52ac2ca8dc8b7b4d
Anton Nikolaev <regwebghost@yandex.ru>
2020-02-19 03:09:04 +0700

    Add markers file creation command

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
+# Create markers file if it not exists
+if [ ! -e ./map/static/data/markers.json ]; then
+  echo "{}" > ./map/static/data/markers.json
+fi
+
+# Create database file if it not exists
 if [ ! -e database.db ]; then
 	touch ./database.db
 fi
 
+# Create required directories
 mkdir -p ./results/
 mkdir -p ./map/static/data/
 
+# Run it all
 docker-compose up -d map
 docker-compose run grinder

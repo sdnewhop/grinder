707edb3a69dcae883dce6225f6376e999d59d95a
Anton Nikolaev <regwebghost@yandex.ru>
2020-02-20 00:20:03 +0700

    Merge master

---
+++ docker/grinder/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+export PS1="(grinder-docker) $PS1"
+
+./grinder.py -h
+
+sh
--- docker_run.sh
+++ docker_run.sh
@@ -1,46 +1,19 @@
 #!/usr/bin/env sh
 
-usage() {
-	printf "Usage: %s [--cpus <quantity-of-cpu>] [-h | --help]\n" "$(basename "$0")"
-}
-
-user_cpu_count=1
-cpu_count=0
-pwd=$(pwd)
-
-while [ $# -gt 0 ]; do
-	case "$1" in
-		"-h"|"--help") usage; exit 0 ;;
-		"--cpus") user_cpu_count=$2; shift 2 ;;
-		*) usage; exit 0 ;;
-	esac
-done
-
-case "$(uname)" in
-	"Linux") cpu_count=$(grep -c 'processor' /proc/cpuinfo) ;;
-	"Darwin") cpu_count=$(sysctl -n hw.logicalcpu) ;;
-	*) cpu_count=1 ;;
-esac
-
-if [ $user_cpu_count -gt $cpu_count ] || [ $cpu_count -eq 1 ]; then
-	user_cpu_count=$cpu_count
+# Create markers file if it not exists
+if [ ! -e ./map/static/data/markers.json ]; then
+  echo "[]" > ./map/static/data/markers.json
 fi
 
-if [ $user_cpu_count -eq 0 ]; then
-	user_cpu_count=$(echo "$cpu_count / 2" | bc)
-fi
-
+# Create database file if it not exists
 if [ ! -e database.db ]; then
 	touch ./database.db
 fi
 
+# Create required directories
 mkdir -p ./results/
 mkdir -p ./map/static/data/
 
-docker run -it --rm \
-	-p 5000:5000 \
-	--cpus="$user_cpu_count" \
-	--volume "${pwd}/database.db:/app/database.db" \
-	--volume "${pwd}/results:/app/results" \
-	--volume "${pwd}/map/static/data:/app/map/static/data" \
-	grinder-framework
+# Run it all
+docker-compose up -d map
+docker-compose run --name grinder_framework grinder

c144cdfe7043c80369dcb9dbe8d1303ea7a15cbf
Anton Nikolaev <regwebghost@yandex.ru>
2020-03-02 23:10:28 +0700

    Update map: searching by keywords + on-map hosts clustering (#83)
    * Add basic interface template for map
    
    * Map clusterization, advanced filtering
    
    * Little updates
    
    * Merge master
    
    * Update map backend
    
    * Update app
    
    * Formatted with Black

--- map/app.py
+++ map/app.py
@@ -1,15 +1,32 @@
-from flask import Flask, send_from_directory, jsonify, json, wrappers
+from flask import (
+    Flask,
+    send_from_directory,
+    jsonify,
+    wrappers,
+    request,
+    redirect,
+)
 from pathlib import Path
-from sys import exit
 from json import load
 from platform import system
 from subprocess import run, PIPE, TimeoutExpired
 
-
-MARKERS: list = []
-
 app = Flask(__name__)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+
+class StorageData:
+    MARKERS: list = []
+    SEARCH_MARKERS: list = []
+    MARKERS_DIR: str = "data"
+    MARKERS_FILE: str = "markers.json"
+
+
+class ReloadFiles:
+    MARKERS: str = str(
+        Path(f"./static/{StorageData.MARKERS_DIR}/{StorageData.MARKERS_FILE}")
+    )
 
 
 def ping(host: str) -> bool:
@@ -20,11 +37,16 @@ def ping(host: str) -> bool:
     """
     param = "-n" if system().lower() == "windows" else "-c"
     command = ["ping", param, "1", host]
-    return run(command, stdout=PIPE, stderr=PIPE, timeout=3).returncode == 0
+    try:
+        return run(command, stdout=PIPE, stderr=PIPE, timeout=5).returncode == 0
+    except:
+        return False
 
 
 @app.before_first_request
-def load_markers(path: str = "data", filename: str = "markers.json") -> None:
+def load_markers(
+    path: str = StorageData.MARKERS_DIR, filename: str = StorageData.MARKERS_FILE
+) -> None:
     """
     Load JSON with markers
     :param path: path to directory with markers
@@ -35,15 +57,11 @@ def load_markers(path: str = "data", filename: str = "markers.json") -> None:
         with open(
             Path(".").joinpath("static").joinpath(path).joinpath(filename), mode="r"
         ) as markers_json:
-            markers_list = load(markers_json)
+            StorageData.MARKERS = load(markers_json)
     except FileNotFoundError:
         print(
             "File with markers not found. Please, finish some scan and run server again."
         )
-        exit(1)
-
-    global MARKERS
-    MARKERS = markers_list
     print(" * Map markers was successfully loaded")
 
 
@@ -76,8 +94,9 @@ def api_raw_host(host_id: str or int) -> wrappers.Response:
     :param host_id: id of host in list of hosts
     :return: flask response
     """
+    markers = StorageData.SEARCH_MARKERS or StorageData.MARKERS
     try:
-        return jsonify(MARKERS[int(host_id)])
+        return jsonify(markers[int(host_id)])
     except IndexError:
         return jsonify({"error": "request index is out of range"})
     except TypeError:
@@ -93,13 +112,12 @@ def api_raw_host_ping(host_id: str or int) -> wrappers.Response:
     :param host_id: id of host in list of hosts
     :return: flask response
     """
+    markers = StorageData.SEARCH_MARKERS or StorageData.MARKERS
     try:
-        host_info = MARKERS[int(host_id)]
-        ip = host_info.get("ip")
-        if ping(ip) == True:
+        host_info = markers[int(host_id)]
+        if ping(host_info.get("ip")):
             return jsonify({"status": "online"})
-        else:
-            return jsonify({"status": "offline"})
+        return jsonify({"status": "offline"})
     except (TimeoutExpired, TimeoutError):
         return jsonify({"error": "timeout"})
     except:
@@ -112,21 +130,55 @@ def api_raw_all() -> wrappers.Response:
     Return full list of hosts in JSON
     :return: flask response
     """
+    markers = StorageData.SEARCH_MARKERS or StorageData.MARKERS
     try:
-        return jsonify(MARKERS)
+        return jsonify(markers)
     except:
         return jsonify({"error": "unexpected error was happened"})
 
 
 @app.route("/", methods=["GET"])
-def root():
+def root() -> wrappers.Response:
     """
     Serve root/index page
     :return: flask response
     """
+    StorageData.SEARCH_MARKERS = []
+    return app.send_static_file("index.html")
+
+
+@app.route("/update", methods=["GET"])
+def api_update_data() -> wrappers.Response:
+    """
+    Update JSON with markers
+    :return: wrappers.Response object
+    """
+    load_markers()
+    return redirect("/")
+
+
+@app.route("/reset", methods=["GET"])
+def reset_search() -> wrappers.Response:
+    """
+    Reset search filter
+    :return: wrappers.Response object
+    """
+    return redirect("/")
+
+
+@app.route("/search", methods=["GET"])
+def search() -> wrappers.Response:
+    """
+    Search for some specific keywords in results
+    :return: wrappers.Response object
+    """
+    query = request.args.get("query", default="", type=str)
+    StorageData.SEARCH_MARKERS = [
+        host for host in StorageData.MARKERS if query.lower() in str(host).lower()
+    ]
     return app.send_static_file("index.html")
 
 
 if __name__ == "__main__":
     load_markers()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, extra_files=[ReloadFiles.MARKERS])

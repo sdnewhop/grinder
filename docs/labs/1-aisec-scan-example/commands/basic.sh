aed5d2e99814927ea24b902506f008f9ba5f6aa3
manmolecular <regwebghost@yandex.ru>
2020-06-27 12:49:23 +0700

    Add scan commands

---
+++ docs/labs/1-aisec-scan-example/commands/basic.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+python3 ./grinder.py -r -u -q queries/aisec_aifinger_all.json -cu -cp -ni
---
+++ docs/labs/1-aisec-scan-example/commands/faster.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+python3 ./grinder.py -r -u -q queries/aisec_aifinger_all.json -cu -cp -ni -sm 50 -cm 50 -vc certain -qc certain
